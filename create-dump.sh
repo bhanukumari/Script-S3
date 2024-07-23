a80ccb10543c4c6abf0eceb1eba90a4ccb5329e3
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-07-23 01:44:11 -0600

    Update create-dump.sh

--- create-dump.sh
+++ create-dump.sh
@@ -3,7 +3,7 @@ stamp=$(date +"%Y_%m_%d_%I_%M_%p")
 #Year_Month_Day_Hour_Minute_AM/PM claculate currect date 
 filename="$stamp-buildpiper-backup.sql"
 #file name created by this type (2024_07_22_03_15_PM-buildpiper-backup.sql)
-mysqldump -ubuildpiper -pbuildpiper@789 buildpiper > /root/backup_script/"$filename"
+mysql -u root -ppassword buildpiper  > /backup_script/"$filename"
 # output is redirected to a file in with the filename variable
 /usr/local/bin/aws s3 cp /root/backup_script/"$filename" s3://buildpiper-nxtpe-backup/
 # This command uploads the backup file to an Amazon S3 bucket named is buildpiper-nxtpe-backup
