bac91c18563d347e81c3f5fa9537c4d95594c80e
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-07-24 11:51:23 +0530

    Update create-dump.sh

--- create-dump.sh
+++ create-dump.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+# Date stamp for filename
 stamp=$(date +"%Y_%m_%d_%I_%M_%p")
-#Year_Month_Day_Hour_Minute_AM/PM claculate currect date 
 filename="$stamp-buildpiper-backup.sql"
-#file name created by this type (2024_07_22_03_15_PM-buildpiper-backup.sql)
-mysql -u root -ppassword buildpiper  > /backup_script/"$filename"
-# output is redirected to a file in with the filename variable
-/usr/local/bin/aws s3 cp /root/backup_script/"$filename" s3://buildpiper-nxtpe-backup/
-# This command uploads the backup file to an Amazon S3 bucket named is buildpiper-nxtpe-backup
+maturity_filename="$stamp-maturity-backup.sql"
+# Dump MySQL database
+docker exec -i db mysqldump -uroot -ppassword buildpiper > /home/opstree/sql_backups/"$filename"
+docker exec -i db mysqldump -uroot -ppassword maturity_dashboard >  /home/opstree/sql_backups/"$maturity_filename"
+aws s3 cp "/home/ubuntu/sql_backups/$filename" s3://bp-23/
+aws s3 cp "/home/ubuntu/sql_backups/$maturity_filename"  s3://bp-23/
+
+# Delete files older than 5 days
+find /home/ubuntu/sql_backups/ -type f -mtime +5 -delete
+
 echo -e "\e[1;32m DB Backup Done $stamp \e[00m"
-#  command is used to print text or variables to the terminal this line indicate the complited the database backup and code sets the text color is gree.
